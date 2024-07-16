5607e9564b4c915db97864a23ae604717cad62af
Paul Pessoa <paulmspessoa@gmail.com>
2024-07-16 15:41:11 -0300

    adjust paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        sans: ['Work Sans', 'sans-serif'],
+      },
+      colors: {
+        'custom-pink': '#FFF4F6', 
+      },
     },
   },
   plugins: [],
