0923bcb05d36ef25e57c9fefb514f2839d01b7d1
Paul Pessoa <paulmspessoa@gmail.com>
2024-07-16 23:38:52 -0300

    rename service

---
+++ src/services/psiService.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { IPsi } from "@/types/psicologo";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+
+export async function getPsiList(): Promise<IPsi[]> {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar lista dos psicólogos:", error);
+    throw error;
+  }
+}
+
+export async function getPsiDetail(id: string): Promise<IPsi> {
+  try {
+    const response = await axios.get(`${API_URL}/${id}`);
+    if (response.status === 404) {
+      throw new Error("Psicólogo não encontrado");
+    }
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao buscar dados do psicólogo:", error);
+    throw error;
+  }
+}
