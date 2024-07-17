01981ce95348c73f5ac0d4fa58f98977dffd1062
Paul Pessoa <paulmspessoa@gmail.com>
2024-07-16 23:35:39 -0300

    adjust tailwind config variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,10 +14,35 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
-        sans: ['Work Sans', 'sans-serif'],
+        sans: ["Work Sans", "sans-serif"],
       },
       colors: {
-        'custom-pink': '#FFF4F6', 
+        primary: {
+          500: "#E9656B",
+          600: "#FF6D73",
+          700: "#FA9DA3",
+          800: "#FCD4DA",
+          900: "#FFE6EB",
+        },
+        secondary: {
+          500: "#002A47",
+          600: "#18527A",
+          700: "#87BBE0",
+          800: "#BCE3FF",
+          900: "#D1ECFF",
+        },
+        gray: "#64696D",
+      },
+      fontSize: {
+        xs: ["10px", { lineHeight: "1.2" }],
+        sm: ["12px", { lineHeight: "1.4" }],
+        base: ["14px", { lineHeight: "1.6" }],
+        lg: ["16px", { lineHeight: "1.8" }],
+        xl: ["18px", { lineHeight: "2" }],
+        "2xl": ["24px", { lineHeight: "1.2" }], // Heading 4
+        "3xl": ["32px", { lineHeight: "1.2" }], // Heading 3
+        "4xl": ["40px", { lineHeight: "1.2" }], // Heading 2
+        "5xl": ["48px", { lineHeight: "1.2" }], // Heading 1
       },
     },
   },
